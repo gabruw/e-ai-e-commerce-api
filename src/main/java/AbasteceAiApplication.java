afef3ccc721c6eeeea5ba0bf7e73054619239b29
gabruw <gabrielmarques.mg@gmail.com>
2021-02-04 10:41:21 -0300

    proj: Projeto base e configurações

---
+++ src/main/java/AbasteceAiApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbasteceAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbasteceAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compasso/uol/gabriel/AbasteceAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compasso.uol.gabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbasteceAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
